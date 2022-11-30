49edbf42b4b285554af7b8a81f0435de0f768f6f
sh2nk <nekkt.sh@gmail.com>
2022-11-30 15:07:42 +0300

    errors to json

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
+
+class InvalidAPIUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        super().__init__()
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
 
 app = Flask(__name__)
 app.config.from_object(os.environ.get("FLASK_ENV") or "config.DevConfig")
 
 db = SQLAlchemy(app)
 migrate = Migrate(app,  db)
+
+@app.errorhandler(InvalidAPIUsage)
+def invalid_api_usage(e):
+    return jsonify(e.to_dict()), e.status_code
 
 from . import dbsync
 from . import api
