409c98eda310d4047da66014bc86a8292e0e7d9c
sh2nk <nekkt.sh@gmail.com>
2022-11-30 06:15:31 +0300

    upload file endpoint

--- app/api/upload.py
+++ app/api/upload.py
@@ -1,6 +1,65 @@
-from app import app
+from app import app, db
+from app.models import File
+from flask import request, abort, jsonify
+from werkzeug.utils import secure_filename
+import os, uuid
+
+class Filename:
+    def __init__(self, name='') -> None:
+        if type(name) == str:
+            self.__name = name
+            self.__ext = name.rsplit('.', 1)[1].lower()
+        else:
+            raise ValueError("Filename should be string!")
+
+    def __call__(self, *args, **kwds) -> str:
+        return self.__name
+
+    def getName(self):
+        return self.__name
+
+    def getExt(self):
+        return ".%s" % self.__ext
+
+    def getID(self):
+        return "%s.%s" % str(uuid.uuid4()), self.__ext
+
+    def isDisabled(self):
+        return '.' in self.__name and self.__ext in app.config["DISABLED_EXTENSIONS"]
+
 
 # Upload file
-@app.route("/file", methods=["POST"])
-def uploadFile():
-    return "file uploader"
+@app.route("/file/<comment>", methods=["POST"])
+def uploadFile(comment=""):
+    if "file" not in request.files:
+        abort(400, description="Missing file part")
+        
+    raw = request.files["file"]
+    secure = secure_filename(raw.filename)
+    filename = Filename(secure)
+
+    if filename() == '':
+        abort(400, description="Empty filename")
+
+    if not raw:
+        abort(400, description="Empty file part")
+
+    if not filename.isDisabled():
+        fileid = filename.getID
+        path = os.path.join(app.config['UPLOAD_FOLDER'], fileid)
+        
+        os.save(path)
+        size = os.stat(path).st_size
+
+        file = File(name=filename.getName, \
+                    extension=filename.getExt, \
+                    size=size, \
+                    path=path, \
+                    comment=comment)
+
+        db.session.add(file)
+        db.session.commit()
+    else:
+        abort(400, description="This file extension is disabled")
+    
+    return jsonify({"message": "Added %s succsesfully" % secure})
--- config.py
+++ config.py
@@ -4,7 +4,9 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BasicConfig:
     DEBUG = False
+    DISABLED_EXTENSIONS = {"html", "php"}
 
 class DevConfig(BasicConfig):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("DB_URI") or "postgres://user:password@localhost:5432/orbis-file"
+    UPLOAD_FOLDER = os.environ.get("UPLOAD_FOLDER") or "./__storage__/"
